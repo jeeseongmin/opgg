78b4797670e0c10e4940d43716e2eb543ea3d4fd
jeeseongmin <peration0422@exntu.com>
2023-12-15 14:36:24 +0900

    feat : Home 관련 파일에 대한 디렉토리 구조 변경

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import 'pages/Home/Home.scss';
+import MainLogo from 'components/Home/MainLogo';
+import SearchComponent from 'components/Home/SearchComponent';
+
+const Index = () => {
+	return (
+		<div className={'homeWrapper'}>
+			<div className={'home'}>
+				<MainLogo />
+				<SearchComponent />
+			</div>
+		</div>
+	);
+};
+
+export default Index;
