d9c7f62f4d98b99fd34b56408dd8b14027a5bb90
jeeseongmin <peration0422@exntu.com>
2023-12-19 09:23:29 +0900

    feat : 홈 내 드롭다운으로부터의 소환사 페이지 링크 생성

--- src/components/Home/Search/SummonerPreviewRow.jsx
+++ src/components/Home/Search/SummonerPreviewRow.jsx
@@ -1,27 +1,30 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
-const SummonerPreviewRow = ({ data }) => {
-	const { game_name, level, profile_image_url, tagline, solo_tier_info } = data;
-	return <div className={'previewRow'}>
-		<div className={'profileImageWrapper'}>
-			<img className={'profileImage'} src={profile_image_url} />
-		</div>
-		<div className={'profileInfoWrapper'}>
-			<p>
-				<span className={'gameName'}>{game_name}</span>
-				<span className={'level'}>#{tagline}</span>
-			</p>
-			<p>
-				{
-					solo_tier_info ? <span>
+const SummonerPreviewRow = ({data}) => {
+	const {game_name, level, profile_image_url, tagline, solo_tier_info} = data;
+	return <Link to={`/summoners/${game_name + '-' + tagline}`}>
+		<div className={'previewRow'}>
+			<div className={'profileImageWrapper'}>
+				<img className={'profileImage'} src={profile_image_url} />
+			</div>
+			<div className={'profileInfoWrapper'}>
+				<p>
+					<span className={'gameName'}>{game_name}</span>
+					<span className={'level'}>#{tagline}</span>
+				</p>
+				<p>
+					{
+						solo_tier_info ? <span>
 						{solo_tier_info.tier + ' ' + solo_tier_info.division + ' - ' + solo_tier_info.lp + 'LP'}
 					</span> : <span>
 						Level {level}
 					</span>
-				}
-			</p>
+					}
+				</p>
+			</div>
 		</div>
-	</div>;
+	</Link>;
 };
 
 export default SummonerPreviewRow;
