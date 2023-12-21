ba2afd9f5458f8e3254723e648fce0350add3823
jeeseongmin <peration0422@exntu.com>
2023-12-21 15:01:33 +0900

    refactor : Summoner 서비스 내 try catch 적용

--- src/services/Summoner.js
+++ src/services/Summoner.js
@@ -2,18 +2,20 @@ import axios from 'axios';
 
 export const SummonerService = {
 	getSummonersByName: async (summonerName) => {
-		return await axios.get(`https://op.gg/api/v1.0/internal/bypass/summoners/v2/kr/autocomplete?gameName=${summonerName}`, {
-				headers: {
-					'content-type': 'application/json;charset=UTF-8',
-					accept: 'application/json,',
-					'Access-Control-Allow-Origin': '*',
+		try {
+			const {data} = await axios.get(
+				`https://op.gg/api/v1.0/internal/bypass/summoners/v2/kr/autocomplete?gameName=${summonerName}`,
+				{
+					headers: {
+						'content-type': 'application/json;charset=UTF-8',
+						accept: 'application/json,',
+						'Access-Control-Allow-Origin': '*',
+					},
 				},
-			},
-		)
-			.then(function(data) {
-				return data.data;
-			}).catch((e) => {
-				return e;
-			});
+			);
+			return data.data;
+		} catch (error) {
+			console.log('error : ', error);
+		}
 	},
 };
