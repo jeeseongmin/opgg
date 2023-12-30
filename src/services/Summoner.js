37eb2a5efde5326b459c856ccce52d3096def955
jeeseongmin <peration0422@exntu.com>
2023-12-31 01:14:32 +0900

    feat : 소환사 정보 관련 서비스 구성

--- src/services/Summoner.js
+++ src/services/Summoner.js
@@ -18,46 +18,50 @@ export const SummonerService = {
 			console.log('error : ', error);
 		}
 	},
-	
-	getAllSummonerByName: async (summonerName) => {
-		return await axios.get(`https://www.op.gg/_next/data/Pz5yb-wmaXIHLSY2-ZUo9/en_US/summoners/search.json?q=${summonerName}&region=kr
-`, {
-				headers: {
-					'content-type': 'application/json;charset=UTF-8',
-					accept: 'application/json,',
-					'Access-Control-Allow-Origin': '*',
-				},
-			},
-		)
-			.then(function(data) {
-				return data.data;
-			}).catch((e) => {
-				return e;
-			});
-	},
+
 	getSummonerByPUUID: async (puuid) => {
-		const {data} = await axios.get(`/kr/lol/summoner/v4/summoners/by-puuid/${puuid}`, {
-			headers: {
-				'X-Riot-Token': process.env.REACT_APP_RIOT_API_KEY,
-			},
-		})
-			.then((data) => {
-				return data;
-			});
-		return data;
+		try {
+			const {data} = await axios
+				.get(`/kr/lol/summoner/v4/summoners/by-puuid/${puuid}`, {
+					headers: {
+						'X-Riot-Token': process.env.REACT_APP_RIOT_API_KEY,
+					},
+				})
+				.then((data) => {
+					return data;
+				});
+			return data;
+		} catch (error) {
+			console.log(error);
+		}
 	},
-	/**
-	 * 소환사의 전적 리스트
-	 */
-	getGameHistoryByAccountId: async (accountID) => {
-		const {data} = await axios.get(`https://kr.api.riotgames.com/lol/match/v4/matchlists/by-account/${accountID}`, {
-			headers: {
-				'X-Riot-Token': process.env.REACT_APP_RIOT_API_KEY,
-			},
-		})
-			.then((data) => {
-				return data;
+
+	getHistory: async (puuid) => {
+		try {
+			const {data} = await axios.get(
+				`/asia/lol/match/v5/matches/by-puuid/${puuid}/ids?start=${0}&count=${20}`,
+				{
+					headers: {
+						'X-Riot-Token': process.env.REACT_APP_RIOT_API_KEY,
+					},
+				},
+			);
+			return data;
+		} catch (error) {
+			console.log(error);
+		}
+	},
+
+	getMatchInfo: async (matchID) => {
+		try {
+			const {data} = await axios.get(`/asia/lol/match/v5/matches/${matchID}`, {
+				headers: {
+					'X-Riot-Token': process.env.REACT_APP_RIOT_API_KEY,
+				},
 			});
-		return data;
+			return data;
+		} catch (error) {
+			console.log(error);
+		}
 	},
 };
