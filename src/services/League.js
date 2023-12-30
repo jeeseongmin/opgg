dca5fe8e24d3b48dbb4167f889a6d0b2a301daf6
jeeseongmin <peration0422@exntu.com>
2023-12-31 01:12:30 +0900

    feat : 리그 정보 관련 서비스 구성

--- src/services/League.js
+++ src/services/League.js
@@ -2,11 +2,18 @@ import axios from 'axios';
 
 export const LeagueService = {
 	getLeagueEntriesById: async (summonerID) => {
-		const { data } = await axios.get(`/kr/lol/league/v4/entries/by-summoner/${summonerID}`, {
-			headers: {
-				'X-Riot-Token': process.env.REACT_APP_RIOT_API_KEY,
-			},
-		});
-		return data;
+		try {
+			const {data} = await axios.get(
+				`/kr/lol/league/v4/entries/by-summoner/${summonerID}`,
+				{
+					headers: {
+						'X-Riot-Token': process.env.REACT_APP_RIOT_API_KEY,
+					},
+				},
+			);
+			return data;
+		} catch (error) {
+			console.log(error);
+		}
 	},
 };
