e6f8941d7d55407c61087f19d8614496fc6e31c9
jeeseongmin <peration0422@exntu.com>
2023-12-31 10:28:08 +0900

    refactor : api 리팩토링

--- src/services/Account.js
+++ src/services/Account.js
@@ -3,23 +3,15 @@ import axios from 'axios';
 export const AccountService = {
 	getAccountByNameAndTag: async (summonerName, tagLine) => {
 		try {
-			const {puuid} = await axios
-				.get(
-					`/asia/riot/account/v1/accounts/by-riot-id/${summonerName}/${tagLine}`,
-					{
-						headers: {
-							'X-Riot-Token': process.env.REACT_APP_RIOT_API_KEY,
-						},
+			const {data} = await axios.get(
+				`/asia/riot/account/v1/accounts/by-riot-id/${summonerName}/${tagLine}`,
+				{
+					headers: {
+						'X-Riot-Token': process.env.REACT_APP_RIOT_API_KEY,
 					},
-				)
-				.then(function(data) {
-					return data.data;
-				})
-				.catch((e) => {
-					return e;
-				});
-
-			return puuid;
+				},
+			);
+			return data.puuid;
 		} catch (error) {
 			console.log(error);
 		}
